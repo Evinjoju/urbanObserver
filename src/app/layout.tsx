867b49c69cdf9679d2eced4c8d5db584a4c8ce58
Evinjoju <evinjoju777@gmail.com>
2025-12-05 11:21:55 +0530

    8th 10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,19 +68,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-
         <meta name="google-site-verification" content="bR-eiO8rI3epTbJIowoHjcJcjkDw-65UGbGr6deTk_0" />
-
         <link rel="icon" href="/images/fin-favIcon.svg" type="image/svg+xml"></link>
 
-
-
-
         {/* Favicon */}
-
         <link rel="apple-touch-icon" href="/images/fin-favIcon.svg" />
-
-
       </head>
       <body className="bg-black text-white antialiased max-w-7xl mx-auto">
         {children}
